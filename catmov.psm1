1626abfa40e9b41bb367ed3fa39d36860cd3b4ec
lopital_win <majikyu.co@gmail.com>
2020-07-26 23:51:13 +0900

    catmov v1

---
+++ catmov.psm1
@@ -0,0 +1,99 @@
+function catmov([parameter(mandatory=$true)][string]$movname, $out = $movname, $start, $end, $sep, $ext = "mp4", [switch]$copy, [switch]$alp){
+
+#####error check 1
+
+	if($alp){
+		if($start -ne $null){
+			if($start.Length -ne 1){
+				echo '%%Error%%  -start:Cannot get String over 2 char'
+				return
+			}
+			if([byte][char]"A" -gt [byte][char]$start -or [byte][char]"z" -lt [byte][char]$start){
+				echo '%%Error%%  -start:Put only A to Z and a to z'
+				return
+			}
+		}
+		if($end -ne $null){
+			if($end.Length -ne 1){
+				echo '%%Error%%  -end:Cannot get String over 2 char'
+				return
+			}
+			if([byte][char]"A" -gt [byte][char]$end -or [byte][char]"z" -lt [byte][char]$end){
+				echo '%%Error%%  -end:Put only A to Z and a to z'
+				return
+			}
+		}
+	}
+	
+	if(!$alp){
+		if($start -ne $null){
+			if(0 -gt $end){
+				echo '%%Error%%  -start:Put unsigned int'
+				return
+			}
+		}if($end -ne $null){
+			if(0 -gt $start){
+				echo '%%Error%%  -end:Put unsigned int'
+				return
+			}
+		}
+	}
+
+#####prepare variables
+
+	if(Test-Path .catmanifest){rm .catmanifest -Force}
+	if($alp){
+		if($start -eq $null){$start=[byte][char]"A"}
+		else{$start=[byte][char]$start}
+	}else{
+		if($start -eq $null){$start = 1}
+	}
+	if($alp -and $end -ne $null){$end = [byte][char]$end}
+	$index = $start
+	
+	
+#####count concat files
+
+	$exist
+	$filenum=0
+	Do
+	{
+	if($alp){$filepath = $movname + $sep + [char]$index + "." + $ext}
+	else{$filepath = $movname + $sep + $index + "." + $ext}
+	$exist = Test-Path $filepath
+		if($exist){$filenum++
+		$index++
+		}		
+	}
+	while($exist -and ($end -eq $null -or $filenum -lt $end - $start + 1) )
+	$index = $start
+#####error check 2
+	
+	if($filenum -lt 2){
+		echo "%%Error%%  cannot find over 2 videos"
+		return
+	}
+	
+	
+#####make manifest file
+
+	for($i=0;$i -lt $filenum;$i++){
+	if($alp){$line = "file " + $movname + $sep + [char]$index + "." + $ext}
+	else{$line = "file " + $movname + $sep + $index + "." + $ext}
+	$line | Out-File .catmanifest -encoding default -append
+	$index++
+	}
+	Set-ItemProperty -path ".\.catmanifest" -name Attributes -value "Hidden"
+
+
+#####execute ffmpeg concat
+
+	$outfilename = $out + "." + $ext
+	if($copy){
+		ffmpeg -f concat -i .catmanifest -c copy $outfilename
+	}else{
+		ffmpeg -f concat -i .catmanifest $outfilename
+	}
+	rm .catmanifest -Force
+	
+}
